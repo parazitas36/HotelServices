6506d7a7327867ca7421ac90a8a08aafc6b75521
Marius <mariukas1666@gmail.com>
2021-11-07 21:26:45 +0200

    ll

---
+++ Pages/PayBillCash.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelServices.Pages
+{
+    public class PayBillCashModel : PageModel
+    {
+        private readonly ILogger<CancelReservationModel> _logger;
+
+        public PayBillCashModel(ILogger<CancelReservationModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
