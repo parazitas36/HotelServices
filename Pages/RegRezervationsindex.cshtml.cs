7eb5fffc0caab810625e293cc95afed4c8b49258
Marius <mariukas1666@gmail.com>
2021-11-07 16:21:47 +0200

    b

---
+++ Pages/RegRezervationsindex.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelServices.Pages
+{
+    public class RegRezervationsindexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public RegRezervationsindexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
