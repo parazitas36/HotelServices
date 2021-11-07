f04b50eb25d8cf7a9736c74b885dbfcbac3d2ee3
Lukas <Lukas@LAPTOP-BLSM8Q6L>
2021-11-07 14:35:19 +0200

    Add project files.

---
+++ Pages/Clientindex.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelServices.Pages
+{
+    public class ClientindexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public ClientindexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
