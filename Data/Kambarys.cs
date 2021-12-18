36b24edd4f8bdf436c14bdd2eb4cefe749be5b6b
Marius <mariukas1666@gmail.com>
2021-12-18 14:19:53 +0200

    aaa

---
+++ Data/Kambarys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelServices.Data
+{
+    public class Kambarys
+    {
+        public int Nr { get; set; }
+        public string Statusas { get; set; }
+    }
+}
--- Pages/Registration/RegRoomsindex.cshtml.cs
+++ Pages/Registration/RegRoomsindex.cshtml.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelServices.Controllers;
+using HotelServices.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,14 +16,33 @@ namespace HotelServices.Pages
     public class RegRoomsindexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
-        public RegRoomsindexModel(ILogger<IndexModel> logger)
+        private readonly IConfiguration _config;
+        private SqlConnection dbc;
+        public List<Kambarys> kambariai;
+        public RegRoomsindexModel(ILogger<IndexModel> logger, IConfiguration config)
         {
             _logger = logger;
+
+            _config = config;
+            DBController db = new DBController(_config);
+            dbc = db.ConnectToDB();
+
+            string query = @"Select Kambarys.nr as Nr, Kambario_statusai.name as statusas from Kambarys, Kambario_statusai 
+where Kambarys.statusas = Kambario_statusai.id_Kambario_statusai";
+            SqlCommand cmd = new SqlCommand(query, dbc);
+            SqlDataReader reader = cmd.ExecuteReader();
+            
+            while (reader.Read()) 
+            {
+                kambariai = new List<Kambarys>();
+                
+            }
+
         }
 
         public void OnGet()
         {
+           
 
         }
     }
